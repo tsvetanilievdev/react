c697395173bf052e873d1027bce677e4ee5f7915
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-04-28 09:27:54 +0300

    update Profile Patient

--- src/components/Main/MyProfilePatient/MyProfilePatient.js
+++ src/components/Main/MyProfilePatient/MyProfilePatient.js
@@ -5,8 +5,10 @@ import { useContext } from 'react';
 import AuthContext from '../../../context/AuthContext.js';
 
 const MyProfileUser = (props) => {
-    const { profile } = useContext(AuthContext)
+    const { profile } = useContext(AuthContext);
+    console.log(profile);
     const eventStatus = 'PAST';
+    const eventConfirmed = false;
 
     return (
         <section className='section__myProfileUser'>
@@ -18,7 +20,7 @@ const MyProfileUser = (props) => {
                     <h3 className="details__info__name">{profile.name}</h3>
                     <p className="details__info__age">Age: {profile.age}</p>
                     <p className="details__info__City">City: {profile.city}</p>
-                    <Link to={`/profile-patient/edit/jdjsajdajsdjas`} className='form__btn light-green'>Edit</Link>
+                    <Link to={`/profile-patient/edit/${profile.id}`} className='form__btn light-green'>Edit</Link>
                 </div>
 
                 <ul className="booking-list myProfileUser">
@@ -30,6 +32,7 @@ const MyProfileUser = (props) => {
                                 <p>Date: 25-04-2023</p>
                                 <p>Hour: 09:30</p>
                                 <p>Reason: First</p>
+                                <p class={`booking-item__info__confirm ${eventConfirmed ? 'confirmed' : 'pending'}`} >{eventConfirmed ? 'Confirmed' : 'Pending'}</p>
                                 <p className='booking-item__info__status'>Status: {eventStatus === 'PAST' ? 'PAST' : 'UPCOMING'}</p>
                             </div>
                             <div className="booking-item__actions">
